05c70419b3bf768414c7b60ae1f127f5b82f4f3b
Parsa Samadnejad <troddenspade@gmail.com>
2022-07-15 00:55:58 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,8 @@
-from Environment import DroneEnv
+from envs.DroneEnv import DroneEnv
 import numpy as np
 
-from stable_baselines3 import PPO
-from stable_baselines3.ppo.policies import CnnPolicy
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.env_checker import check_env
+import ICM
 
-N_TIMESTEPS = 10000
-env = DroneEnv(env_path='./envs/1.csv')
+agent = ICM.PPO(DroneEnv, n_steps=128,  tensorboard_log="./logs/")
 
-check_env(env)
-
-model = PPO(CnnPolicy, env, verbose=1, n_steps=512, tensorboard_log="./logs/")
-
-for i in range(20):
-    model.learn(total_timesteps=N_TIMESTEPS, 
-                eval_env=None, 
-                eval_freq=N_TIMESTEPS, 
-                n_eval_episodes=5, 
-                tb_log_name='PPO', 
-                eval_log_path='./logs/',
-                reset_num_timesteps=False)
-        
-    model.save(f'./models/PPO/{(i+1)*N_TIMESTEPS}')
+agent.train(total_timesteps=10_000)
