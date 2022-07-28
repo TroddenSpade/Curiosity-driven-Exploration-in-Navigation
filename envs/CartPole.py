0f65dba60a7cefdb45c7a0ad77b7f2911ccc142c
Parsa Samadnejad <troddenspade@gmail.com>
2022-07-28 12:33:57 +0430

    Create CartPole.py

---
+++ envs/CartPole.py
@@ -0,0 +1,55 @@
+import gym
+
+class CartPole(gym.Env):
+    def __init__(self):
+        self.env = gym.make("CartPole-v1")
+        self.steps = 0
+        self.MAX_STEPS = 500
+
+        self.action_space = self.env.action_space
+        self.observation_space = self.env.observation_space
+
+
+    def render(self):
+        self.env.render()
+
+
+    def reset(self):
+        self.steps = 0
+        self.reward = 0.0
+        state = self.env.reset()
+        return state
+
+
+    def step(self, action):
+        self.steps += 1
+        extrinsic_reward = 0
+        state, rew, done, info = self.env.step(action)
+        self.reward += rew
+        if done:
+            if self.steps < self.MAX_STEPS:
+                extrinsic_reward = -100
+            else:
+                extrinsic_reward = 100
+        return state, extrinsic_reward, done, info
+
+
+    def episode(self):
+        return self.reward, self.steps
+
+
+if __name__ == '__main__':
+    env = CartPole()
+    state = env.reset()
+
+    while True:
+        action = env.action_space.sample()
+        print(action)
+        state, reward, done, info = env.step(action)
+        env.render()
+        print(reward)
+        if done:
+            state = env.reset()
+            print("Done")
+            break
+        
