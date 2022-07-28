17b95d39afe101a07083eed87ed32ee69738a326
Parsa Samadnejad <troddenspade@gmail.com>
2022-07-28 12:33:50 +0430

    Create cp-main.py

---
+++ cp-main.py
@@ -0,0 +1,30 @@
+from matplotlib import pyplot as plt
+import torch.nn as nn
+
+import ICM
+from envs.CartPole import CartPole
+
+env = CartPole()
+
+policy_kwargs = {
+    "hidden_layers": (32,),
+}
+
+value_kwargs = {
+    "hidden_layers": (32,)
+}
+
+icm_kwargs = {
+    "inverse_hidden_layer": (), 
+    "forward_hidden_layer": (),
+    "activation_fn": nn.ReLU
+}
+
+agent = ICM.PPO(env, n_steps=256, 
+                policy_kwargs=policy_kwargs, 
+                value_kwargs=value_kwargs, 
+                icm_kwargs=icm_kwargs,
+                use_fe=False,
+                feature_size=32, tensorboard_log="./logs/")
+
+agent.train(total_timesteps=100_000)
