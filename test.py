c87751c652b0d58f9e90869252fe33dee5766162
Parsa Samadnejad <troddenspade@gmail.com>
2022-07-30 00:48:16 +0430

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,9 @@ from filecmp import cmp
 from cv2 import waitKey
 import matplotlib.pyplot as plt
 
-from Environment import DroneEnv
+from envs.DroneEnv import DroneEnv
 
-env = DroneEnv()
+env = DroneEnv('./envs/1.csv')
 
 state = env.reset()
 
@@ -13,7 +13,7 @@ for k in range(16):
     print("forward")
     state, r, d, i = env.step([1, 0.0])
     print(r, d, i)
-    plt.imsave(f'{k}.png', state)
+    # plt.imsave(f'{k}.png', state)
 
 for k in range(2):
     print("right")
@@ -21,7 +21,7 @@ for k in range(2):
     print(r, d)
     plt.imsave(f'{k}.png', state)
 
-for _ in range(7):
+for _ in range(12):
     print("forward")
     state, r, d, _ = env.step([1, 0.0])
     print(r, d)
@@ -32,6 +32,16 @@ for _ in range(2):
     print(r, d)
 
 for _ in range(3):
+    print("forward")
+    state, r, d, _ = env.step([1, 0.0])
+    print(r, d)
+
+for _ in range(2):
+    print("right")
+    state, r, d, _ = env.step([1, 1])
+    print(r, d)
+
+for _ in range(7):
     print("forward")
     state, r, d, _ = env.step([1, 0.0])
     print(r, d)
@@ -47,34 +57,32 @@ for _ in range(3):
     print(r, d)
 
 for _ in range(2):
-    print("right")
-    state, r, d, _ = env.step([1, 1])
-    print(r, d)
-
-for _ in range(4):
-    print("forward")
-    state, r, d, _ = env.step([1, 0.0])
-    print(r, d)
-
-for _ in range(2):
-    print("right")
-    state, r, d, _ = env.step([1, 1])
-    print(r, d)
-
-for _ in range(4):
-    print("forward")
-    state, r, d, _ = env.step([1, 0.0])
-    print(r, d)
-
-for _ in range(2):
     print("left")
-    state, r, d, i = env.step([1, -1])
+    state, r, d, _ = env.step([1, -1])
+    print(r, d)
+
+for _ in range(6):
+    print("forward")
+    state, r, d, _ = env.step([1, 0.0])
+    print(r, d)
+
+for _ in range(2):
+    print("right")
+    state, r, d, i = env.step([1, 1])
+    print(r, d, i)
+
+for _ in range(2):
+    print("forward")
+    state, r, d, i = env.step([1, 0.0])
+    print(r, d, i)
+
+for _ in range(2):
+    print("right")
+    state, r, d, i = env.step([1, 1])
     print(r, d, i)
 
 for _ in range(3):
     print("forward")
     state, r, d, i = env.step([1, 0.0])
     print(r, d, i)
-
-
 exit()
