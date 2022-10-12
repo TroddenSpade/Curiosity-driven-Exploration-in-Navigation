d65a5d374b8ae5d2e9f747953f8a291bc2f53f24
Parsa Samadnejad <troddenspade@gmail.com>
2022-10-13 02:07:38 +0330

    Update CNN.py

--- nn/CNN.py
+++ nn/CNN.py
@@ -8,22 +8,20 @@ import torch.nn.functional as F
     input: [None, 144, 256, 1]; output: [None, 1344] -> [None, 256];
 '''
 class FeatureExtractor(nn.Module):
-    def __init__(self, output_size=256):
+    def __init__(self, output_size=64):
         super().__init__()
 
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(3, 8, kernel_size=(5, 5), stride=(4, 4)),
-            nn.ELU(),
-            nn.Conv2d(8, 16, kernel_size=(3, 3), stride=(2, 2)),
-            nn.ELU(),
-            nn.Conv2d(16, 32, kernel_size=(3, 3), stride=(2, 2)),
-            nn.ELU(),
-            nn.Conv2d(32, 64, kernel_size=(3, 3), stride=(2, 2)),
-            nn.ELU(),
+            nn.Conv2d(4, 32, kernel_size=(8, 8), stride=(4, 4)),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=(4, 4), stride=(2, 2)),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=(3, 3), stride=(1, 1)),
+            nn.ReLU(),
             nn.Flatten(start_dim=1, end_dim=-1),
 
-            nn.Linear(in_features=1344, out_features=output_size, bias=True),
-            nn.ELU(),
+            nn.Linear(in_features=3136, out_features=output_size, bias=True),
+            nn.ReLU(),
         )
 
     def forward(self, state):
@@ -33,7 +31,7 @@ class FeatureExtractor(nn.Module):
 
 
 if __name__ == "__main__":
-    state = torch.randn(1, 3, 144, 256)
+    state = torch.randn(1, 4, 84, 84)
     feature_extractor = FeatureExtractor()
     x = feature_extractor(state)
     print(x.shape)
